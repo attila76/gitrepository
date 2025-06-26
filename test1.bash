5e3bfbff20e6ca20be119f6aae14eb6aa0e05526
ASZILVAS <atszilvas@gmail.com>
2025-06-26 10:02:48 +0200

    ezt is

---
+++ test1.bash
@@ -0,0 +1,41 @@
+A szülönktől kapott információs rendszer ami alaőján döntést hozunk
+magunkra
+a világra 
+a kapcsolatainkra 
+és a jövönkre vonatkozóan
+
+ez is jó lesz
+- Járt utat a járatlanért ne adj!
+
+Anyám szerint az élet:
+
+
+
+Apám szerint az élet:
+
+
+
+Nem tehetem meg:
+
+
+
+Evidenciákat: 
+
+
+
+Rendes ember képe:
+
+
+
+Ha te sikeres leszel, mit fog ehhez anyukád, apukád szólni?
+
+
+PROFILOZÁS:
+
+
+ Boxer LOSES EVERY FIGHT On Purpose
+ 
+ 61018a8bcc1d470d86fd51c8a6775687
+ 
+ mvn archetype:generate -DgroupId=com.expedia.cli -DartifactId=flightsbookingcli -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+

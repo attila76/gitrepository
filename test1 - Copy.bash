78131be52fd57632a698b52cd8e672f07b0ce1f6
Attila <atszilvas@gmail.com>
2025-06-26 15:51:42 +0200

    oké 1

---
+++ test1 - Copy.bash
@@ -0,0 +1,41 @@
+A szülönktől kapott információs rendszer ami alapján döntést hozunk
+magunkra
+a világra 
+a kapcsolatainkra 
+és a jövönkre vonatkozóan
+
+
+- Járt utat a járatlanért ne adj!
+
+Anyám szerint az élet:
+érdekes
+
+
+Apám szerint az élet:
+
+
+
+Nem tehetem meg:
+
+
+
+Evidenciákat: 
+
+
+
+Rendes ember képe:
+
+
+
+Ha te sikeres leszel, mit fog ehhez anyukád, apukád szólni?
+
+
+PROFILOZÁS:
+
+
+ Boxer LOSES EVERY FIGHT On Purpose
+ 
+ 61018a8bcc1d470d86fd51c8a6775687
+ 
+ mvn archetype:generate -DgroupId=com.expedia.cli -DartifactId=flightsbookingcli -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
